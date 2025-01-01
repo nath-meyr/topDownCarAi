9799e0f78c1de963eab9e9da4d7447efe7c44c09
NathMey <meyer.nathan@outlook.be>
2025-01-01 18:51:35 +0100

    feat: wip

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 // Debug flag
 const DEBUG = true;
 let gameWorld;
-let track;
-let car;
 
 function setup() {
     createCanvas(GameWorld.CANVAS_WIDTH, GameWorld.CANVAS_HEIGHT);
@@ -16,36 +14,64 @@ function setup() {
     CAR_WHEEL_IMAGE = loadImage(Car.CAR_WHEEL_IMAGE_PATH);
     gameWorld.loadAssets();
 
-    // Initialize track and car
-    track = new Track(gameWorld.getPhysicsWorld(), DEBUG);
+    // Initialize track in gameWorld
+    const track = new Track(gameWorld.getPhysicsWorld(), DEBUG);
     track.setup();
+    gameWorld.setTrack(track);
 
-    car = new Car(gameWorld, DEBUG);
-    car.setTotalCheckpoints(track.getTotalCheckpoints());
+    // Create cars and store them in gameWorld
+    for (let i = 0; i < GameWorld.CARS_COUNT; i++) {
+        const car = new Car(gameWorld, DEBUG);
+        car.setTotalCheckpoints(track.getTotalCheckpoints());
+        car.setPosition(0, 0);
+        car.setCarIndex(i);
+        gameWorld.addCar(car);
+    }
+}
+function keyPressed() {
+    if (gameWorld.selectionMode) {
+        if (key === ' ') {
+            gameWorld.cycleFocusedCar();
+        } else if (keyCode === ENTER) {
+            gameWorld.startNextGeneration();
+        } else if (key === 'B' || key === 'b') {
+            gameWorld.focusOnBestCar();
+        } else if (key === 'A' || key === 'a') {
+            gameWorld.toggleAutoSelect();
+        }
+    } else {
+        if (key === ' ') {
+            gameWorld.cycleFocusedCar();
+        } else if (keyCode === ENTER) {
+            gameWorld.startRaceCountdown();
+        } else if (key === 'S' || key === 's') {
+            gameWorld.enterSelectionMode();
+        } else if (key === 'B' || key === 'b') {
+            gameWorld.focusOnBestCar();
+        } else if (key === 'A' || key === 'a') {
+            gameWorld.toggleAutoSelect();
+        }
+    }
 }
 
 function draw() {
-    // Get car speed for scale factor calculation
-    const carSpeed = Math.sqrt(
-        Math.pow(car.chassisBody.velocity[0], 2) +
-        Math.pow(car.chassisBody.velocity[1], 2)
-    );
+    // Update race state
+    gameWorld.updateRaceState();
 
-    // Update scale factor based on speed and race state
-    gameWorld.updateScaleFactor(carSpeed, car.raceFinished);
+    // Get average car speed for scale factor calculation
+    const averageSpeed = gameWorld.getAverageCarSpeed();
+
+    // Update scale factor based on average speed and race state
+    const anyCarFinished = gameWorld.isAnyCarFinished();
+    gameWorld.updateScaleFactor(averageSpeed, anyCarFinished);
 
     // Step the physics world
     gameWorld.step();
 
-    push();
-    scale(gameWorld.getScaleFactor());
+    // Update all cars
+    gameWorld.updateCars();
 
-    car.moveViewToCar();
-    gameWorld.drawGrass();
-
-    track.draw();
-    car.draw();
-
-    pop();
+    // Draw the world (includes all visual elements)
+    gameWorld.draw();
 }
 
